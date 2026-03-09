8b7e1177ab5c6ae1d7902ac1f82a8a75823d6474
cediackermann <ackermanncedi@gmail.com>
2026-03-09 08:29:23 +0100

    Better error handling in db.js and retry logic improvement

--- fw/db.js
+++ fw/db.js
@@ -3,43 +3,48 @@ const dbConfig = require("../config");
 
 // Verbindung zur Datenbank herstellen
 async function connectDB() {
-  try {
-    const connection = await mysql.createConnection(dbConfig);
-    const MAX_RETRIES = 5;
-    const RETRY_DELAY_MS = 2000;
-    let retries = 0;
+  const MAX_RETRIES = 5;
+  const RETRY_DELAY_MS = 2000;
+  let retries = 0;
 
-    while (retries < MAX_RETRIES) {
-      try {
-        const connection = await mysql.createConnection(dbConfig);
-        console.log("Database connected");
-        return connection;
-      } catch (error) {
-        retries++;
-        console.error(
-          `Error connecting to database (attempt ${retries}/${MAX_RETRIES}):`,
-          error.message,
+  while (retries < MAX_RETRIES) {
+    try {
+      const connection = await mysql.createConnection(dbConfig);
+      console.log("Database connected");
+      return connection;
+    } catch (error) {
+      retries++;
+      console.error(
+        `Error connecting to database (attempt ${retries}/${MAX_RETRIES}):`,
+        error.message,
+      );
+      if (retries < MAX_RETRIES) {
+        console.log(`Retrying in ${RETRY_DELAY_MS / 1000} seconds...`);
+        await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
+      } else {
+        throw new Error(
+          "Failed to connect to database after multiple retries: " +
+            error.message,
         );
-        if (retries < MAX_RETRIES) {
-          console.log(`Retrying in ${RETRY_DELAY_MS / 1000} seconds...`);
-          await new Promise((resolve) => setTimeout(resolve, RETRY_DELAY_MS));
-        } else {
-          throw new Error(
-            "Failed to connect to database after multiple retries.",
-          );
-        }
       }
     }
-    return connection;
-  } catch (error) {
-    console.error("Error connecting to database:", error);
   }
 }
 
 async function executeStatement(statement) {
-  let conn = await connectDB();
-  const [results, fields] = await conn.query(statement);
-  return results;
+  let conn;
+  try {
+    conn = await connectDB();
+    const [results, fields] = await conn.query(statement);
+    return results;
+  } catch (error) {
+    console.error("Error executing statement:", error.message);
+    throw error;
+  } finally {
+    if (conn) {
+      await conn.end();
+    }
+  }
 }
 
 module.exports = { connectDB: connectDB, executeStatement: executeStatement };
